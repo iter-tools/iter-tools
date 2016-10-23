cef79a1421526c52743ea41d73fc23b5430a56c8
Maurizio Lupo <maurizio.lupo@gmail.com>
2016-10-23 08:46:53 +0100

    fixed issue with module names

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 var iterators = require('require-all')(__dirname + '/lib');
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function toLowerCapitalize(str) { // from zip-longest to zipLongest
+  return str.split('-').map((s, index) => {
+    if (index) {
+      return capitalize(s);
+    }
+    return s;
+  })
+  .join('');
+}
+
+Object.keys(iterators)
+.forEach(function (name) {
+  var alternateName = toLowerCapitalize(name);
+  if (name !== alternateName) {
+    iterators[alternateName] = iterators[name];
+  }
+});
+
 module.exports = iterators;
