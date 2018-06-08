32c2125c8cf2e35b25ed9c09d81d203536bf26ca
George Maddocks <gmaddocks1@gmail.com>
2018-06-08 11:33:27 +0100

    Rename interior function

--- src/async-batch.mjs
+++ src/async-batch.mjs
@@ -1,6 +1,6 @@
 import asyncIter from './async-iter'
 export default function batch (number, iterable) {
-  async function * curriedBatch (_iterable) {
+  async function * curriedAsyncBatch (_iterable) {
     let batch = []
     for await (const item of asyncIter(_iterable)) {
       batch.push(item)
@@ -15,7 +15,7 @@ export default function batch (number, iterable) {
   }
 
   if (iterable) {
-    return curriedBatch(iterable)
+    return curriedAsyncBatch(iterable)
   }
-  return curriedBatch
+  return curriedAsyncBatch
 }
