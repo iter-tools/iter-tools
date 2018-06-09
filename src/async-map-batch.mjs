c3a27db8975dfb8b8bf4edb87c578657173e4944
Maurizio Lupo <maurizio.lupo@gmail.com>
2018-06-09 22:33:46 +0100

    async-map-batch (2)

--- src/async-map-batch.mjs
+++ src/async-map-batch.mjs
@@ -1,6 +1,6 @@
 import asyncBatch from './async-batch'
 
-export default function asyncMapBatch(number, func, iterable) {
+export default function asyncMapBatch (number, func, iterable) {
   async function * curriedAsyncMapBatch (_iterable) {
     for await (const items of asyncBatch(number, _iterable)) {
       const results = await Promise.all(items.map(func))
