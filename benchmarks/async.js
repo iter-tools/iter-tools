4d02855ac37222c57cc9fb5137440ace49457b38
Maurizio Lupo <maurizio.lupo@gmail.com>
2018-07-08 21:48:46 +0100

    fixed another lint bug

--- benchmarks/async.js
+++ benchmarks/async.js
@@ -10,11 +10,10 @@ function asyncf () {
   const f2 = asyncFilter((n) => n % 2)
 
   measureSpeed(function () {
-    let sum = 0
+    let sum = 0 // eslint-disable-line
     for (const n of f1(range(1000))) {
       sum += n
     }
-    return sum
   }, { samples: 1000, discard: 10 }, function (err, ms) {
     if (err) return console.log('Error!')
     console.log('************ sync ************')
@@ -22,11 +21,10 @@ function asyncf () {
   })
 
   measureSpeed(async function () {
-    let sum = 0
+    let sum = 0 // eslint-disable-line
     for await (const n of f2(range(1000))) { // eslint-disable-line
       sum += n
     }
-    return sum
   }, { samples: 1000, discard: 10 }, function (err, ms) {
     if (err) return console.log('Error!')
     console.log('************ async ************')
