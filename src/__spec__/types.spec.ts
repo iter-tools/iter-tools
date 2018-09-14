0525aeafb692a5730acf46c1c4d5d748fde7676d
khai96_ <hvksmr1996@gmail.com>
2018-09-14 16:09:27 +0700

    Use simpler assertion syntax

--- src/__spec__/types.spec.ts
+++ src/__spec__/types.spec.ts
@@ -3,11 +3,14 @@ import * as tuple from "typescript-tuple";
 import * as iter from "../index";
 import compare = assert.compare;
 
-const a = iter.combinations([0, 1, 2, 3], 3);
-compare<typeof a, Iterable<[number, number, number]>>("equal");
+assert<
+  Iterable<[number, number, number]>
+>(iter.combinations([0, 1, 2, 3], 3));
 
-const b = iter.permutations([0, 1, 2, 3], 3);
-compare<typeof b, Iterable<[number, number, number]>>("equal");
+assert<
+  Iterable<[number, number, number]>
+>(iter.permutations([0, 1, 2, 3], 3));
 
-const c = iter.product([0, 1, 2], [3, 4, 5]);
-compare<typeof c, Iterable<number[]>>("equal");
+assert<
+  Iterable<number[]>
+>(iter.product([0, 1, 2], [3, 4, 5]));
