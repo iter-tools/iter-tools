e23bdb701f343a1fcaeaf2794e4b1fb6a9f79153
khai96_ <hvksmr1996@gmail.com>
2018-09-14 14:58:24 +0700

    Add test

--- src/__spec__/types.spec.ts
+++ src/__spec__/types.spec.ts
@@ -6,8 +6,14 @@ import compare = assert.compare;
 const a = iter.combinations([0, 1, 2, 3], 3);
 compare<typeof a, Iterable<[number, number, number]>>("equal");
 
+const a0 = iter.combinations([0, 1, 2, 3], Number());
+compare<typeof a0, Iterable<number[]>>("equal");
+
 const b = iter.permutations([0, 1, 2, 3], 3);
 compare<typeof b, Iterable<[number, number, number]>>("equal");
+
+const b0 = iter.permutations([0, 1, 2, 3], Number());
+compare<typeof b0, Iterable<number[]>>("equal");
 
 const c = iter.product([0, 1, 2], [3, 4, 5]);
 compare<typeof c, Iterable<number[]>>("equal");
