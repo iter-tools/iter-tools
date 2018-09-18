0f9e5bccaf372cfc92acf67524d1e72d90993a23
Khải <hvksmr1996@gmail.com>
2018-09-18 14:30:47 +0700

    [TypeScript] Fix some types and update tests (#67)
    * Fix some types
    
    * Make type tests more specific

--- src/__spec__/permutations.spec.ts
+++ src/__spec__/permutations.spec.ts
@@ -2,25 +2,25 @@ import assert from "static-type-assert";
 import * as iter from "../index";
 
 assert<
-  Iterable<[number, number, number]>
+  IterableIterator<[number, number, number]>
 >(iter.permutations([0, 1, 2, 3], 3));
 
 assert<
-  Iterable<number[]>
+  IterableIterator<number[]>
 >(iter.permutations([0, 1, 2, 3], Number()));
 
 assert<
-  Iterable<number[]>
+  IterableIterator<number[]>
 >(iter.permutations([0, 1, 2, 3], 999));
 
 assert<
-  Iterable<[string, string, string]>
+  IterableIterator<[string, string, string]>
 >(iter.permutations(iter.iterable(""), 3));
 
 assert<
-  Iterable<[number, number, number, number]>
+  IterableIterator<[number, number, number, number]>
 >(iter.permutations([0, 1, 2, 3] as [number, number, number, number]));
 
 assert<
-  Iterable<string[]>
+  IterableIterator<string[]>
 >(iter.permutations(iter.iterable("")));
--- src/__spec__/product.spec.ts
+++ src/__spec__/product.spec.ts
@@ -2,18 +2,18 @@ import assert from "static-type-assert";
 import * as iter from "../index";
 
 assert<
-  Iterable<[0 | 1 | 2, 3 | 4 | 5]>
+  IterableIterator<[0 | 1 | 2, 3 | 4 | 5]>
 >(iter.product(
   [0, 1, 2] as [0, 1, 2],
   [3, 4, 5] as [3, 4, 5]
 ));
 
 assert<
-  Iterable<[number, number, number]>
+  IterableIterator<[number, number, number]>
 >(iter.product([0, 1, 2], [3, 4, 5], [7, 8, 9]));
 
 assert<
-  Iterable<[string, string, string]>
+  IterableIterator<[string, string, string]>
 >(iter.product(
   iter.iterable(""),
   iter.iterable(""),
