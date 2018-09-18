4d9c8cb18ffb722b3ccc38597f12f7a582e65435
khai96_ <hvksmr1996@gmail.com>
2018-09-18 12:09:57 +0700

    Make type tests more specific

--- src/__spec__/execute.spec.ts
+++ src/__spec__/execute.spec.ts
@@ -2,33 +2,33 @@ import assert from "static-type-assert";
 import * as iter from "../index";
 
 assert<
-  Iterable<123>
+  IterableIterator<123>
 >(iter.execute(() => 123 as 123));
 
 assert<
-  Iterable<123>
+  IterableIterator<123>
 >(iter.execute((x) => x, 123 as 123, 456 as 456));
 
 assert<
-  Iterable<[]>
+  IterableIterator<[]>
 >(iter.execute((...args) => args));
 
 assert<
-  Iterable<[0, 1, 2]>
+  IterableIterator<[0, 1, 2]>
 >(iter.execute((...args) => args, 0 as 0, 1 as 1, 2 as 2));
 
 assert<
-  AsyncIterable<123>
+  AsyncIterableIterator<123>
 >(iter.asyncExecute(async () => 123 as 123));
 
 assert<
-  AsyncIterable<123>
+  AsyncIterableIterator<123>
 >(iter.asyncExecute(async (x) => x, 123 as 123, 456 as 456));
 
 assert<
-  AsyncIterable<[]>
+  AsyncIterableIterator<[]>
 >(iter.asyncExecute(async (...args) => args));
 
 assert<
-  AsyncIterable<[0, 1, 2]>
+  AsyncIterableIterator<[0, 1, 2]>
 >(iter.asyncExecute(async (...args) => args, 0 as 0, 1 as 1, 2 as 2));
--- src/__spec__/range.spec.ts
+++ src/__spec__/range.spec.ts
@@ -2,13 +2,13 @@ import assert from "static-type-assert";
 import * as iter from "../index";
 
 assert<
-  Iterable<0 | 1 | 2>
+  IterableIterator<0 | 1 | 2>
 >(iter.range(3));
 
 assert<
-  Iterable<number>
+  IterableIterator<number>
 >(iter.range(200));
 
 assert<
-  Iterable<number>
+  IterableIterator<number>
 >(iter.range({start: 2}));
