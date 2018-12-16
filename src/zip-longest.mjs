9068b1b697195c0443b339492f1f3a052c5d953d
sithmel <maurizio.lupo@gmail.com>
2018-12-16 18:20:06 +0000

    fix typo

--- src/zip-longest.mjs
+++ src/zip-longest.mjs
@@ -4,17 +4,17 @@ export default function * zipLongest (...iterables) {
   const iters = iterables.map(i => ensureIterable(i)[Symbol.iterator]())
   try {
     while (true) {
-      let numberOfExausted = 0
+      let numberOfExhausted = 0
       const zipped = new Array(iterables.length)
       let i = 0
       for (const iter of iters) {
         const { done, value } = iter.next()
         if (done) {
-          numberOfExausted++
+          numberOfExhausted++
         }
         zipped[i++] = done ? undefined : value
       }
-      if (iters.length === numberOfExausted) {
+      if (iters.length === numberOfExhausted) {
         return
       }
       yield zipped
