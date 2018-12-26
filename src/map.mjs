7251d4a2635a1e8bb4f1fb8a3b03108ae5c37e55
sithmel <maurizio.lupo@gmail.com>
2018-12-26 16:36:22 +0000

    reverted optimisation on map

--- src/map.mjs
+++ src/map.mjs
@@ -1,17 +1,6 @@
 import ensureIterable from './internal/ensure-iterable'
 
-function mapArray(func, arrayLike) {
-  const out = new Array(arrayLike.length)
-  for (let i = 0, len = arrayLike.length; i < len; i++) {
-    out[i] = func(arrayLike[i], i)
-  }
-  return out
-}
-
 function * map (func, iterable) {
-  if ('length' in iterable && iterable.length < 10000) {
-    return mapArray(func, iterable)
-  }
   let c = 0
   for (const item of ensureIterable(iterable)) {
     yield func(item, c++)
--- src/size.mjs
+++ src/size.mjs
@@ -1,6 +1,8 @@
 import ensureIterable from './internal/ensure-iterable'
 
 export default function size (iterable) {
+  if (Array.isArray(iterable)) return iterable.length
+  if (iterable instanceof Map || iterable instanceof Set) return iterable.size
   let size = 0
   // eslint-disable-next-line
   for (const _ of ensureIterable(iterable)) {
