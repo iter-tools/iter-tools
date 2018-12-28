24a103c5758a7404185f6b86db2f06aa79467414
sithmel <maurizio.lupo@gmail.com>
2018-12-28 23:06:41 +0000

    added async benchmarks, and some minor fix

---
+++ benchmarks/async-map-1000.js
@@ -0,0 +1,87 @@
+const range = require('../es2018/range')
+const asyncMap = require('../es2018/async-map')
+const asyncReduce = require('../es2018/async-reduce')
+
+const { Readable, Transform } = require('stream')
+
+function power2 (x) {
+  return x * x
+}
+
+function concat (acc, n) {
+  return acc.concat(n)
+}
+
+const a = Array.from(range(1000))
+
+const numbersLimit = 1000
+const fastNumbers = () => {
+  let num = 0
+  return new Readable({
+    objectMode: true,
+    read () {
+      if (num > numbersLimit) {
+        this.push(null)
+        return
+      }
+      this.push(++num)
+    }
+  })
+}
+
+const streamMap = (fn, options = {}) => new Transform({
+  // By default we are in object mode but this can be overwritten by the user
+  objectMode: true,
+  ...options,
+
+  transform (chunk, encoding, callback) {
+    let res
+    try {
+      res = fn(chunk)
+    } catch (e) {
+      return callback(e)
+    }
+    callback(null, res)
+  }
+})
+
+const streamReduce = (fn, acc, options = {}) => new Transform({
+  objectMode: true,
+  ...options,
+
+  transform (chunk, encoding, callback) {
+    try {
+      acc = fn(acc, chunk)
+    } catch (e) {
+      return callback(e)
+    }
+    return callback()
+  },
+
+  flush (callback) {
+    callback(null, acc)
+  }
+})
+
+module.exports['stream map 1000 items'] = {
+  fn: async function (deferred) {
+    const stream = fastNumbers()
+      .pipe(streamMap(power2))
+      .pipe(streamReduce(concat, []))
+
+    stream.on('data', (array) => {
+      deferred.resolve()
+    })
+  },
+  defer: true
+}
+
+module.exports['iter-tools async map 1000 items'] = {
+  fn: async function (deferred) {
+    const iter = asyncMap(power2)
+
+    await asyncReduce([], concat, iter(a))
+    deferred.resolve()
+  },
+  defer: true
+}
--- src/size.mjs
+++ src/size.mjs
@@ -1,8 +1,11 @@
 import ensureIterable from './internal/ensure-iterable'
 
+const TypedArray = Object.getPrototypeOf(Int8Array)
+
 export default function size (iterable) {
   if (Array.isArray(iterable)) return iterable.length
   if (iterable instanceof Map || iterable instanceof Set) return iterable.size
+  if (Object.getPrototypeOf(iterable) === TypedArray) return iterable.length
   let size = 0
   // eslint-disable-next-line
   for (const _ of ensureIterable(iterable)) {
