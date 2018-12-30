a4921ae1b6ed3cfa68e88474977e67f36a6dacc7
khai96_ <hvksmr1996@gmail.com>
2018-12-31 03:16:45 +0700

    Create find.spec.ts

---
+++ src/__spec__/find.spec.ts
@@ -0,0 +1,20 @@
+import assert from 'static-type-assert'
+import * as iter from '../index'
+
+const sampleIterable = [0, 1, 2, 3]
+
+assert<2 | null>(
+  iter.find(
+    (item): item is 2 => item === 2,
+    sampleIterable
+  )
+)
+
+assert<
+  Promise<2 | null>
+>(
+  iter.asyncFind(
+    (item): item is 2 => item === 2,
+    sampleIterable
+  )
+)
