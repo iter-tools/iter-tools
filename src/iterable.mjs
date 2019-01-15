8b81e1bddee3664dfe9822414abca2982f92b114
khai96_ <hvksmr1996@gmail.com>
2019-01-15 15:21:38 +0700

    Revert "Fix: iterable() should always return an iterator"
    This reverts commit 205367b9add24d7f8e14f412a6f4d7d23b5be448.

--- src/iterable.mjs
+++ src/iterable.mjs
@@ -2,9 +2,9 @@ const emptyArray = []
 
 export default function iterable (iterator) {
   if (iterator == null) {
-    return emptyArray[Symbol.iterator]()
+    return emptyArray
   } else if (iterator[Symbol.iterator]) {
-    return iterator[Symbol.iterator]()
+    return iterator
   } else if (typeof iterator !== 'object' || typeof iterator.next !== 'function') {
     throw new Error(`Expected to receive an iterator of the form {next()}, but instead received: ${iterator}`)
   }
