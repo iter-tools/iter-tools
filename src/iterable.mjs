205367b9add24d7f8e14f412a6f4d7d23b5be448
khai96_ <hvksmr1996@gmail.com>
2019-01-15 13:59:08 +0700

    Fix: iterable() should always return an iterator

--- src/iterable.mjs
+++ src/iterable.mjs
@@ -2,9 +2,9 @@ const emptyArray = []
 
 export default function iterable (iterator) {
   if (iterator == null) {
-    return emptyArray
+    return emptyArray[Symbol.iterator]()
   } else if (iterator[Symbol.iterator]) {
-    return iterator
+    return iterator[Symbol.iterator]()
   } else if (typeof iterator !== 'object' || typeof iterator.next !== 'function') {
     throw new Error(`Expected to receive an iterator of the form {next()}, but instead received: ${iterator}`)
   }
