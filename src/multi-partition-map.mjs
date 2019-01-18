454968639c284ef06ab0fbfc69ddffeb255c3556
khai96_ <hvksmr1996@gmail.com>
2019-01-18 17:12:12 +0700

    Refactor multiPartitionMap() to use interal partitionPart()

--- src/multi-partition-map.mjs
+++ src/multi-partition-map.mjs
@@ -1,23 +1,18 @@
 import AdvancedMapInitialized from 'advanced-map-initialized'
 import Dequeue from 'dequeue'
 import ensureIterable from './internal/ensure-iterable'
+import partitionPart from './internal/partition-part'
 
 function uncurried (func, iter) {
   const queueMap = new AdvancedMapInitialized(Map, () => new Dequeue())
   const iterator = ensureIterable(iter)[Symbol.iterator]()
 
-  function * part (queue) {
-    while (true) {
-      while (queue.length) {
-        yield queue.shift()
-      }
-
-      const { value, done } = iterator.next()
-      if (done) break
-
-      queueMap.get(func(value)).push(value)
-    }
-  }
+  const part = queue => partitionPart(
+    iterator,
+    queue,
+    value => queueMap.get(func(value)),
+    () => false
+  )
 
   return new AdvancedMapInitialized(Map, index => part(queueMap.get(index)))
 }
