9c725681439b948cdaccb2cccef00903a7fe68a9
khai96_ <hvksmr1996@gmail.com>
2019-01-23 21:31:43 +0700

    fork() that handles iterator.return()
    Limitation: iterator.return() is supposed to called when all sub-iterators are exhausted,
    but since there's no limit to number of sub-iterators, iterator.return() will be called
    when all accessed sub-iterators are exhausted.

--- src/fork.mjs
+++ src/fork.mjs
@@ -6,20 +6,28 @@ import ensureIterable from './internal/ensure-iterable'
 export default function fork (iterable) {
   const queue = []
   const iterator = ensureIterable(iterable)[Symbol.iterator]()
+  let exhausted = 0
 
   function * part () {
     let cursor = 0
 
-    while (true) {
-      while (cursor < queue.length) {
-        yield queue[cursor]
-        cursor += 1
+    try {
+      while (true) {
+        while (cursor < queue.length) {
+          yield queue[cursor]
+          cursor += 1
+        }
+
+        const { value, done } = iterator.next()
+        if (done) break
+
+        queue.push(value)
       }
-
-      const { value, done } = iterator.next()
-      if (done) break
-
-      queue.push(value)
+    } finally {
+      exhausted++
+      if (exhausted === queue.length) {
+        if (typeof iterator.return === 'function') iterator.return()
+      }
     }
   }
 
