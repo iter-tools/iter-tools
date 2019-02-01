256a80f823128e18a5cbe1244a19e7c2387a1c3e
sithmel <maurizio.lupo@gmail.com>
2019-02-01 07:43:07 +0000

    small tweak on curry

--- src/internal/curry.mjs
+++ src/internal/curry.mjs
@@ -1,6 +1,6 @@
 export default function curry (fn, expectedArgsLength = fn.length, appliedArgs = []) {
   return (...args) => {
-    if (expectedArgsLength - args.length <= 0) {
+    if (expectedArgsLength <= args.length) {
       return fn(...appliedArgs, ...args)
     }
     return curry(fn, expectedArgsLength - args.length, [...appliedArgs, ...args])
