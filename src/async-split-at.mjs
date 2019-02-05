e3aa2c3acdd5aa9087bdd18c0d9ec725e098c707
khai96_ <hvksmr1996@gmail.com>
2019-02-05 11:22:18 +0700

    Attempt to optimize asyncSplitAt

--- src/async-split-at.mjs
+++ src/async-split-at.mjs
@@ -1,14 +1,69 @@
-import asyncTee from './async-tee'
-import asyncSlice from './async-slice'
+import ensureAsyncIterable from './internal/ensure-async-iterable'
 import curry from './internal/curry'
 
-function asyncSplitAt (position, iter) {
-  const [first, second] = asyncTee(iter)
+function asyncSplitAt (index, iterable) {
+  const iter = ensureAsyncIterable(iterable)[Symbol.asyncIterator]()
+  let position = 0
+  let firstCache = null
+  let firstCacheStart = null
+  let done = false
 
-  return [
-    asyncSlice({ end: position }, first),
-    asyncSlice({ start: position }, second)
-  ]
+  async function cleanUp () {
+    if (!done && typeof iter.return === 'function') {
+      await iter.return()
+    }
+  }
+
+  async function * first () {
+    try {
+      while (!done) {
+        if (firstCache) {
+          yield firstCache[position - firstCacheStart]
+        } else {
+          const item = await iter.next()
+          done = item.done
+          if (!done) {
+            yield item.value
+          }
+        }
+        position++
+      }
+    } finally {
+      if (position !== index - 1) {
+        await cleanUp()
+      }
+    }
+  }
+
+  async function * second () {
+    try {
+      if (position < index - 1) {
+        firstCacheStart = position
+        firstCache = new Array(index - position)
+        for (let i = position; i < index; i++) {
+          const item = await iter.next()
+          done = item.done
+          if (!done) {
+            firstCache[i] = item.value
+          }
+        }
+        position = index
+      }
+
+      while (!done) {
+        const item = await iter.next()
+        done = item.done
+        position++
+        if (!done) {
+          yield item.value
+        }
+      }
+    } finally {
+      await cleanUp()
+    }
+  }
+
+  return [first(), second()]
 }
 
 export default curry(asyncSplitAt)
