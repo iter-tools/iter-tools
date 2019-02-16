9cc9136c6ca6037d24ff64f9b21b1c5ab9231145
sithmel <maurizio.lupo@gmail.com>
2019-02-16 21:26:14 +0000

    added test on asyncMultiPartition

--- src/__tests__/multi-partition.test.js
+++ src/__tests__/multi-partition.test.js
@@ -1,8 +1,15 @@
 /* eslint-env node, jest */
-const { multiPartition, range, slice, pipe } = require('..')
+const {
+  multiPartition,
+  asyncMultiPartition,
+  range,
+  slice,
+  toArray,
+  asyncToArray,
+  pipe } = require('..')
 
 describe('multiPartition', function () {
-  const allToArray = (...args) => args.map(iter => Array.from(iter))
+  const allToArray = (...args) => args.map(iter => toArray(iter))
 
   describe('x => x % 4', function () {
     const func = x => x % 4
@@ -29,6 +36,38 @@ describe('multiPartition', function () {
         Array.from
       )(range(16))
       expect(allToArray(...iv)).toEqual([[], []])
+    })
+  })
+})
+
+describe('asyncMultiPartition', function () {
+  const allToArray = (...args) => Promise.all(args.map(iter => asyncToArray(iter)))
+
+  describe('x => x % 4', function () {
+    const func = x => x % 4
+
+    it('empty iterable', async function () {
+      const [a, b, c, d] = asyncMultiPartition(func, [])
+      expect(await allToArray(a, b, c, d)).toEqual(Array(4).fill([]))
+    })
+
+    it('range(16)', async function () {
+      const [a, b, c, d] = asyncMultiPartition(func, range(16))
+      expect(await allToArray(a, b, c, d)).toEqual([
+        [0, 4, 8, 12],
+        [1, 5, 9, 13],
+        [2, 6, 10, 14],
+        [3, 7, 11, 15]
+      ])
+    })
+
+    it('out-of-bound access returns empty iterables', async function () {
+      const iv = pipe(
+        asyncMultiPartition(func),
+        slice({ start: 6, end: 8 }),
+        toArray
+      )(range(16))
+      expect(await allToArray(...iv)).toEqual([[], []])
     })
   })
 })
