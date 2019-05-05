abd39c3a157271058ae59b4414f96f69ad95523e
sithmel <maurizio.lupo@gmail.com>
2019-05-05 14:09:33 +0100

    fixed default argument on asyncFilter

--- src/async-filter.mjs
+++ src/async-filter.mjs
@@ -1,11 +1,7 @@
 import asyncMap from './async-map'
 import { asyncIterableCurry } from './internal/async-iterable'
 
-async function * asyncFilter (concurrency, func, iterable) {
-  if (concurrency == null) {
-    concurrency = 1
-  }
-
+async function * asyncFilter (concurrency = 1, func, iterable) {
   let c = 0
 
   const mapped = asyncMap(concurrency, async item => ({ item, value: await func(item, c++) }), iterable)
