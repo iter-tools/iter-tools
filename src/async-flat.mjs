85074be6bdd1094a32fb1a14540795e750dc0c80
Conrad Buck <conartist6@gmail.com>
2019-05-22 08:50:54 -0700

    Allow object configuration of flat

--- src/async-flat.mjs
+++ src/async-flat.mjs
@@ -2,7 +2,12 @@ import { asyncIterableCurry } from './internal/async-iterable'
 
 const defaultShouldFlat = item => (typeof item[Symbol.iterator] === 'function' || typeof item[Symbol.asyncIterator] === 'function') && typeof item !== 'string'
 
-function flat (shouldFlat = defaultShouldFlat, depth = 1, iterable) {
+function flat (shouldFlat = defaultShouldFlat, depthOrOptions = 1, iterable) {
+  let depth = depthOrOptions
+  if (depthOrOptions && typeof depthOrOptions === 'object') {
+    ({ shouldFlat = defaultShouldFlat, depth = 1 } = depthOrOptions)
+  }
+
   async function * _flat (currentDepth, iterable) {
     for await (const item of iterable) {
       if (currentDepth < depth && (await shouldFlat(item))) {
