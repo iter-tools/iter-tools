cd85fdf0af81a8b99fb3cc27cc63dc5f604563a9
Conrad Buck <conartist6@gmail.com>
2019-08-10 12:02:21 -0700

    Add wrap method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,6 +24,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
  - collate
  - splitAt/asyncSplitAt
  - call, apply
+ - wrap
  - firstOr
  - when
  - isEmpty
---
+++ src/$wrap.d.ts
@@ -0,0 +1,5 @@
+import { $InputIterable, $GeneratorIterator } from './internal/$iterable';
+
+declare function $wrap<T = any>(iterable: $InputIterable<T>): $GeneratorIterator<T>;
+
+export default $wrap;
