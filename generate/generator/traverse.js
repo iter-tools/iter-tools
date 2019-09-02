2880ef0ddb7328f6972669367a2fdc0c0d94662a
Conrad Buck <conartist6@gmail.com>
2019-09-02 09:51:01 -0700

    Fix ignore patterns evaluating against absolute paths

--- generate/generator/traverse.js
+++ generate/generator/traverse.js
@@ -12,13 +12,17 @@ function makeMatcher(root, glob) {
   return path => match(relative(root, path));
 }
 
+function makeIgnoreMatcher(root, pattern) {
+  const isMatch = matcher(pattern);
+  return (path, stats) => stats.isFile() && isMatch(relative(root, path));
+}
+
 function traverse(root, options) {
   const { ignored, glob } = options;
 
   const matchesGlob = makeMatcher(root, glob);
-  const ignoredMatchers = ignored.map(glob => makeMatcher(root, glob));
 
-  return recursive(root, ignoredMatchers).then(initialPaths => {
+  return recursive(root, [makeIgnoreMatcher(root, ignored)]).then(initialPaths => {
     return initialPaths.filter(path => matchesGlob(path)).map(path => relative(root, path));
   });
 }
