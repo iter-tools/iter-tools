d6b62b2b139c1512029d1723ff05a5ad68c403e9
Conrad Buck <conartist6@gmail.com>
2019-09-28 21:14:36 -0700

    Don't log generated gitattributes for gitignored files

--- generate/generators/_templates/gitattributes-file.js
+++ generate/generators/_templates/gitattributes-file.js
@@ -1,10 +1,17 @@
-module.exports = generatedPaths =>
-  `# @generated
+const fs = require('fs');
+const ignore = require('ignore');
+
+module.exports = generatedPaths => {
+  const gitignore = ignore().add(fs.readFileSync('.gitignore', 'utf8'));
+
+  return `# @generated
 # It should not be necessary to edit this file directly.
 # The template for this file is: generate/templates/gitattributes-file.js
 
 ${[...generatedPaths]
+  .filter(path => !gitignore.ignores(path))
   .sort()
   .map(path => `${path} linguist-generated=true`)
   .join('\n')}
 `;
+};
