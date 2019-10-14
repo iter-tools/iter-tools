22bb1f88e729afdfbf991cfdcd022786292b25d1
Conrad Buck <conartist6@gmail.com>
2019-10-14 10:38:18 -0700

    Update package build

--- babel-es2015.config.js
+++ babel-es2015.config.js
@@ -1,21 +1,27 @@
 module.exports = {
-  plugins: [
-    ['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }],
-    '@babel/plugin-proposal-async-generator-functions',
-  ],
-  env: {
-    test: {
-      plugins: [['@babel/plugin-transform-runtime', { corejs: 2 }]],
-    },
-    es: {
-      plugins: ['./babel-plugin-pure-curry'],
-    },
-    cjs: {
+  extends: './babel-shared.config.js',
+  overrides: [
+    {
+      test: '**/*.mjs',
       plugins: [
-        'add-module-exports',
-        '@babel/plugin-transform-modules-commonjs',
-        ['@babel/plugin-transform-runtime', { corejs: 2 }],
+        ['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }],
+        '@babel/plugin-proposal-async-generator-functions',
       ],
+      env: {
+        test: {
+          plugins: [['@babel/plugin-transform-runtime', { corejs: 2 }]],
+        },
+        es: {
+          plugins: ['./babel-plugin-pure-curry'],
+        },
+        cjs: {
+          plugins: [
+            'add-module-exports',
+            '@babel/plugin-transform-modules-commonjs',
+            ['@babel/plugin-transform-runtime', { corejs: 2 }],
+          ],
+        },
+      },
     },
-  },
+  ],
 };
--- babel-es5.config.js
+++ babel-es5.config.js
@@ -1,26 +1,32 @@
 module.exports = {
-  plugins: [['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }]],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-      },
-    ],
-  ],
-  env: {
-    test: {
-      plugins: [['@babel/plugin-transform-runtime', { corejs: 2 }]],
-    },
-    es: {
-      plugins: ['./babel-plugin-pure-curry'],
-    },
-    cjs: {
-      plugins: [
-        'add-module-exports',
-        '@babel/plugin-transform-modules-commonjs',
-        ['@babel/plugin-transform-runtime', { corejs: 2 }],
+  extends: './babel-shared.config.js',
+  overrides: [
+    {
+      test: '**/*.mjs',
+      plugins: [['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }]],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+          },
+        ],
       ],
+      env: {
+        test: {
+          plugins: [['@babel/plugin-transform-runtime', { corejs: 2 }]],
+        },
+        es: {
+          plugins: ['./babel-plugin-pure-curry'],
+        },
+        cjs: {
+          plugins: [
+            'add-module-exports',
+            '@babel/plugin-transform-modules-commonjs',
+            ['@babel/plugin-transform-runtime', { corejs: 2 }],
+          ],
+        },
+      },
     },
-  },
+  ],
 };
