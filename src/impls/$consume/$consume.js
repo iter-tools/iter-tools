c00266dfaa507051435f5e4b72b0269c3d82b301
Conrad Buck <conartist6@gmail.com>
2020-12-10 10:42:47 -0500

    Remove deprecated consume callback

--- src/impls/$consume/$consume.js
+++ src/impls/$consume/$consume.js
@@ -1,35 +1,16 @@
-import { $, $async, $await } from '../../../generate/async.macro.cjs';
+import { $async, $await } from '../../../generate/async.macro.cjs';
 
 import { $iterableCurry } from '../../internal/$iterable.js';
 
-let warnedCallbackDeprecation = false;
-
-const warnCallbackDeprecation = () => {
-  if (!warnedCallbackDeprecation) {
-    console.warn(
-      `\`${$`consume`}(callback, iterable)\` is deprecated and will be removed in iter-tools@8. ` +
-        `Instead use ${$`forEach`}(callback, iterable)`,
-    );
-    warnedCallbackDeprecation = true;
-  }
-};
-
 $async;
-export function $__consume(iterable, callback = () => {}) {
-  let c = 0;
+export function $__consume(iterable) {
   $await;
-  for (const value of iterable) {
-    $await(callback(value, c++));
-  }
+  /* eslint-disable */
+  // prettier-ignore
+  for (const _ of iterable) {}
+  /* eslint-enable */
 }
 
 export const $consume = /*#__PURE__*/ $iterableCurry($__consume, {
   reduces: true,
-  minArgs: 0,
-  maxArgs: 1,
-  validateArgs(args) {
-    if (typeof args[1] === 'function') {
-      warnCallbackDeprecation();
-    }
-  },
 });
