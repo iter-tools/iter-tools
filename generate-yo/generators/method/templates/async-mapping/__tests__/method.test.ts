1a3e08d692d4453579f609f3ea1d1ab5cc096e32
Conrad Buck <conartist6@gmail.com>
2020-12-11 17:27:39 -0500

    Yo: .ts tests and $___method__

---
+++ generate-yo/generators/method/templates/async-mapping/__tests__/method.test.ts
@@ -0,0 +1,11 @@
+import { __method__ } from 'iter-tools-es';
+import { asyncWrap, asyncUnwrap } from '../../../test/async-helpers.js';
+
+describe('__method__', () => {
+  it(
+    'TODO: replace this test',
+    async () => {
+      expect(await asyncUnwrap(__method__(asyncWrap([1, 2, 3])))).toEqual([1, 2, 3]);
+    },
+  );
+});
