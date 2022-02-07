9149084740bdeb0387fadcea9f07072ceb67f233
Conrad Buck <conartist6@gmail.com>
2022-02-07 11:19:13 -0500

    Minor improvements for contributors

---
+++ generate/async.macro.cjs.d.ts
@@ -0,0 +1,7 @@
+export const $: any;
+export const $async: any;
+export const $await: any;
+export const $isAsync: any;
+export const $isSync: any;
+export const $Promise: any;
+export const $MaybePromise: any;
