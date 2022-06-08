84311363bbc7d0b372ff74b731ca3958f793349d
Conrad Buck <conartist6@gmail.com>
2022-06-08 00:25:01 -0400

    Fix equal types

--- src/impls/$equal/$equal.d.ts
+++ src/impls/$equal/$equal.d.ts
@@ -4,15 +4,13 @@ import { $Wrappable } from '../../types/$iterable';
 
 declare function $equal(
   same: (a: any, b: any) => boolean,
-): (...seqs: Array<$Wrappable<any>>) => (iterable: $Wrappable<any>) => $Promise<boolean>;
+): (...seqs: Array<$Wrappable<any>>) => $Promise<boolean>;
 
-declare function $equal(
-  same: (a: any, b: any) => boolean,
-  ...seqs: Array<$Wrappable<any>>
-): (iterable: $Wrappable<any>) => $Promise<boolean>;
+declare function $equal<T>(
+  same: (a: T, b: T) => boolean,
+  ...seqs: Array<$Wrappable<T>>
+): $Promise<boolean>;
 
-declare function $equal(
-  ...seqs: Array<$Wrappable<any>>
-): (iterable: $Wrappable<any>) => $Promise<boolean>;
+declare function $equal(...seqs: Array<$Wrappable<any>>): $Promise<boolean>;
 
 export { $equal };
--- src/impls/$equal/equal.d.ts
+++ src/impls/$equal/equal.d.ts
@@ -10,13 +10,10 @@ import { Wrappable } from '../../types/iterable';
 
 declare function equal(
   same: (a: any, b: any) => boolean,
-): (...seqs: Array<Wrappable<any>>) => (iterable: Wrappable<any>) => boolean;
+): (...seqs: Array<Wrappable<any>>) => boolean;
 
-declare function equal(
-  same: (a: any, b: any) => boolean,
-  ...seqs: Array<Wrappable<any>>
-): (iterable: Wrappable<any>) => boolean;
+declare function equal<T>(same: (a: T, b: T) => boolean, ...seqs: Array<Wrappable<T>>): boolean;
 
-declare function equal(...seqs: Array<Wrappable<any>>): (iterable: Wrappable<any>) => boolean;
+declare function equal(...seqs: Array<Wrappable<any>>): boolean;
 
 export { equal };
